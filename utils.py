5a82e3361d1e9ea6fc9fc2c3da3e5c16a37db4ea
Eugene Shatsky <eugene@shatsky.net>
2014-07-06 09:58:58 +0300

    Added @transaction.commit_on_success to separate journaling database queries from calee view function queries

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from django.db import transaction
+
+@transaction.commit_on_success # to prevent view failures if journal request causes database error
 def journal_event(journal, request, event_dict):
     """Adds journal event"""
     try: journal.objects.create(address=(request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')), agent=request.META.get('HTTP_USER_AGENT'), referer=request.META.get('HTTP_REFERER'), **event_dict)
